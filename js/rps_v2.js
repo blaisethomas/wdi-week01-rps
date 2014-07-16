fdc3e04c10dee29ff70a90c527c761ea8761194d
blaisethomas <blaisegthomas@gmail.com>
2014-07-15 18:57:08 -0700

    updated all, images, positioning, responsive

--- js/rps_v2.js
+++ js/rps_v2.js
@@ -1,21 +1,33 @@
 function user(humanChoice) {
-alert("you chose " + humanChoice)    
+//alert("you chose " + humanChoice)    
 var userChoice = humanChoice;
 var computerChoice = Math.random();
+var x=document.getElementById("compChoice");
+// document.getElementByClassName("border")[0].classList.remove("border");
+document.getElementById("white_rock").classList.remove("border");
+document.getElementById("white_paper").classList.remove("border");
+document.getElementById("white_scissor").classList.remove("border");
 
     if(computerChoice < 0.34) {
 	    computerChoice = "rock";
+        x.innerHTML = "rock";
+        document.getElementById("white_rock").classList.add("border");
     } 
     else if(computerChoice <= 0.67) {
 	    computerChoice = "paper";
+        x.innerHTML = "paper";
+        document.getElementById("white_paper").classList.add("border");
     } 
     else {
 	    computerChoice = "scissors";
-    } alert("Computer chose " + computerChoice);
+        x.innerHTML = "scissors";
+        document.getElementById("white_scissor").classList.add("border");
+    } //alert("Computer chose " + computerChoice);
 
 var compare = function(choice1, choice2) {
     if(choice1 === choice2) {
         alert("The result is a tie");
+        
     }
     else if (choice1 === "rock") {
         if(choice2 === "scissors") {
