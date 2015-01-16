61a0a6f42cf482779210a2150d4df42ea2cea584
blaisethomas <blaisegthomas@gmail.com>
2015-01-15 18:05:17 -0800

    AJAX

--- js/rps_v2.js
+++ js/rps_v2.js
@@ -2,76 +2,68 @@ $(document).ready(function(){
 
     //player move animations
     $("#black_rock").click(function(){
-        $("#black_rock").addClass('border animated rubberBand')
+        $("#black_rock").addClass('animated rubberBand')    
     });
 
     $("#black_scissor").click(function(){
-        $("#black_scissor").addClass('border animated rubberBand')
+        $("#black_scissor").addClass('animated rubberBand')
     });
 
     $("#black_paper").click(function(){
-        $("#black_paper").addClass('border animated rubberBand')
+        $("#black_paper").addClass('animated rubberBand')
     });
 });
 
 function user(humanChoice) {
-
-    console.log("you choose " + humanChoice)   
-
-    //variables
+    console.log("you choose " + humanChoice) 
     var computerChoice = Math.random();
-    var x=document.getElementById("compChoice");
-    var y=document.getElementById("resulting");
+    console.log("computer chooses " + computerChoice);  
 
     //reset animations 
-    //all animations thanks to animate.css => http://daneden.github.io/animate.css/
-    $("#black_rock").removeClass('border animated rubberBand')
-    $("#black_scissor").removeClass('border animated rubberBand')
-    $("#black_paper").removeClass('border animated rubberBand')
-    $("#white_rock").removeClass('border animated rubberBand')
-    $("#white_scissor").removeClass('border animated rubberBand')
-    $("#white_paper").removeClass('border animated rubberBand')
+    //all animations with thanks daneden => http://daneden.github.io/animate.css/
+    $("#black_rock").removeClass();
+    $("#black_scissor").removeClass();
+    $("#black_paper").removeClass();
+    $("#white_rock").removeClass();
+    $("#white_scissor").removeClass();
+    $("#white_paper").removeClass();
 
-    // computer's move
+    // computer's move animations
     if(computerChoice < 0.34) {
 	    computerChoice = "rock";
-        x.innerHTML = "rock";
-        $("#white_rock").addClass('border animated rubberBand')
-        
-        //console.log("rock")
+        $("#white_rock").addClass('animated rubberBand');
     } else if(computerChoice <= 0.67) {
 	    computerChoice = "paper";
-        x.innerHTML = "paper";
-        $("#white_paper").addClass('border animated rubberBand')
+        $("#white_paper").addClass('animated rubberBand');
     } else {
 	    computerChoice = "scissors";
-        x.innerHTML = "scissors";
-        $("#white_scissor").addClass('border animated rubberBand')
-    } console.log("computer chooses " + computerChoice);
+        $("#white_scissor").addClass('animated rubberBand');
+    } 
 
+    // win check
     var compare = function(choice1, choice2) {
         if(choice1 === choice2) {
-            y.innerHTML = "Its a DRAW!";
-            
+            // y.innerHTML = "Its a DRAW!";
+            $("img").addClass('animated wobble')   
         } else if (choice1 === "rock") {
             if(choice2 === "scissors") {
-                y.innerHTML = "YOU WIN";
+                // y.innerHTML = "YOU WIN";
             } else {
-                y.innerHTML = "YOU LOSE";
+                // y.innerHTML = "YOU LOSE";
             }
         } else if (choice1 === "paper") {
             if(choice2 === "rock") {
-                y.innerHTML = "YOU WIN";
+                // y.innerHTML = "YOU WIN";
             }
             else {
-                y.innerHTML = "YOU LOSE";
+                // y.innerHTML = "YOU LOSE";
             }
         } else if (choice1 === "scissors") {
             if(choice2 === "paper") {
-                y.innerHTML = "YOU WIN";
+                // y.innerHTML = "YOU WIN";
             }
             else {
-                y.innerHTML = "YOU LOSE";
+                // y.innerHTML = "YOU LOSE";
             }
         }
     }
