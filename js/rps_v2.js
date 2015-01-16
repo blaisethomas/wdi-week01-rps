3c8109b4dbc85de9a87b02b4e85348c5387355b7
blaisethomas <blaisegthomas@gmail.com>
2015-01-15 16:23:33 -0800

    more JQuery

--- js/rps_v2.js
+++ js/rps_v2.js
@@ -1,55 +1,51 @@
 function user(humanChoice) {
-    //alert("you chose " + humanChoice)    
-    var userChoice = humanChoice;
+
+    console.log("you choose " + humanChoice)   
+    
+    //variables
     var computerChoice = Math.random();
     var x=document.getElementById("compChoice");
     var y=document.getElementById("resulting");
-    // document.getElementByClassName("border")[0].classList.remove("border");
-    document.getElementById("white_rock").classList.remove("border");
-    document.getElementById("white_paper").classList.remove("border");
-    document.getElementById("white_scissor").classList.remove("border");
 
-        if(computerChoice < 0.34) {
-    	    computerChoice = "rock";
-            x.innerHTML = "rock";
-            console.log("rock")
-            //$(document).ready(function(){
-                $("img").click(function(){
-                    $("#white_rock").addClass('animated bounceOutLeft');
-                });
-            //});
-        } else if(computerChoice <= 0.67) {
-    	    computerChoice = "paper";
-            x.innerHTML = "paper";
-            document.getElementById("white_paper").classList.add("animated bounce");
-        } else {
-    	    computerChoice = "scissors";
-            x.innerHTML = "scissors";
-            document.getElementById("white_scissor").classList.add("border");
-        } //alert("Computer chose " + computerChoice);
+    //reset animations 
+    //all animations thanks to animate.css => http://daneden.github.io/animate.css/
+    $("#black_rock").removeClass('border animated flipInX')
+    $("#black_scissor").removeClass('border animated flipInX')
+    $("#black_paper").removeClass('border animated flipInX')
+
+    // computer's move
+    if(computerChoice < 0.34) {
+	    computerChoice = "rock";
+        x.innerHTML = "rock";
+        //console.log("rock")
+    } else if(computerChoice <= 0.67) {
+	    computerChoice = "paper";
+        x.innerHTML = "paper";
+        //document.getElementById("white_paper").classList.add("border animated flipInX");
+    } else {
+	    computerChoice = "scissors";
+        x.innerHTML = "scissors";
+        //document.getElementById("white_scissor").classList.add("border animated flipInX");
+    } //alert("Computer chose " + computerChoice);
 
     var compare = function(choice1, choice2) {
         if(choice1 === choice2) {
             y.innerHTML = "Its a DRAW!";
             
-        }
-        else if (choice1 === "rock") {
+        } else if (choice1 === "rock") {
             if(choice2 === "scissors") {
                 y.innerHTML = "YOU WIN";
-            }
-            else {
+            } else {
                 y.innerHTML = "YOU LOSE";
             }
-        }
-        else if (choice1 === "paper") {
+        } else if (choice1 === "paper") {
             if(choice2 === "rock") {
                 y.innerHTML = "YOU WIN";
             }
             else {
                 y.innerHTML = "YOU LOSE";
             }
-        }
-        else if (choice1 === "scissors") {
+        } else if (choice1 === "scissors") {
             if(choice2 === "paper") {
                 y.innerHTML = "YOU WIN";
             }
@@ -58,5 +54,31 @@ function user(humanChoice) {
             }
         }
     }
-    compare (userChoice, computerChoice)
+    compare (humanChoice, computerChoice)
 }
+
+//animations
+$(document).ready(function(){
+
+
+
+
+    $("#black_rock").click(function(){
+        $("#black_rock").addClass('border animated flipInX')
+    });
+
+    $("#black_scissor").click(function(){
+        $("#black_scissor").addClass('border animated flipInX')
+    });
+
+    $("#black_paper").click(function(){
+        $("#black_paper").addClass('border animated flipInX')
+    });
+
+});
+
+
+
+
+
+
